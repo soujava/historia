a7d6fc81e04f3bde5b05516f0f0a5cf4e58811a0
Cesar Augusto Nogueira <cesarnogueira1210@gmail.com>
2017-06-14 11:19:59 -0300

    Extend time lenght of animations
    Extend time lenght of animations to show blocks

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ jQuery(document).ready(function($){
 	var timelineBlocks = $('.cd-timeline-block'),
 		offset = 0.8;
 
-	//hide timeline blocks which are outside the viewport
+	// hide timeline blocks which are outside the viewport
 	hideBlocks(timelineBlocks, offset);
 
-	//on scolling, show/animate timeline blocks when enter the viewport
+	// on scolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
 		(!window.requestAnimationFrame) 
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 300)
 			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
 	});
 
@@ -20,7 +20,7 @@ jQuery(document).ready(function($){
 
 	function showBlocks(blocks, offset) {
 		blocks.each(function(){
-			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			( $(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
 });
